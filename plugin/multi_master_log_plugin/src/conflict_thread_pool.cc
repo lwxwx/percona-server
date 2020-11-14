be00c8a084f203c5aa98db4c04ab22d2c308c317
pokemonwei <pokemonwei@126.com>
2020-11-14 12:10:04 +0000

    some modify about plugin/multi_master_log_plugin/src/conflict_thread_pool.cc

---
+++ plugin/multi_master_log_plugin/src/conflict_thread_pool.cc
@@ -0,0 +1,90 @@
+#include "conflict_thread_pool.h"
+#include <bits/stdint-uintn.h>
+#include <mutex>
+#include "trx_log.h"
+
+ConflictThreadEnv::ConflictThreadEnv()
+{
+
+}
+
+ConflictThreadEnv:~ConflictThreadEnv()
+{
+
+}
+
+ConflictThreadEnv::operator()()
+{
+	while (1) 
+	{
+		{
+			std::unique_lock<std::mutex> lck(state_mutex);
+			idle_cond.wait(lck,[]{return thread_finish_flag.load() || !task_queue.empty(); });
+		}
+		if(thread_finish_flag.load() && task_queue.empty())
+		{
+			break;
+		}
+
+		ConflictTask * task_ptr = &task_queue.front();
+		task_execute(task_ptr);
+		if(task_ptr->task_finish_cond_ptr != NULL)
+		{
+			task_ptr->task_finish_cond_ptr->notify_all();
+		}
+		task_queue.pop();
+
+	}
+}
+
+void ConflictThreadEnv::task_execute(ConflictTask *task)
+{
+	auto it = task->task_content.begin();
+	bool verify_result = false;
+	for( ; it != task->task_content.end(); it++)
+	{
+		verify_result = (*verifyFunc)(task->cur_log,*it);
+		if(verify_result)
+		{
+			break;
+		}
+	}
+	(*task->task_result) = verify_result;
+}
+
+int ConflictThreadPool::init(uint32_t capacity ,ConflictVerifyFunction * verify_fun)
+{
+	verifyFunc = verify_fun;
+	max_capacity = capacity;
+	thread_vector.resize(capacity);
+	for (int i = 0; i < capacity; i++)
+	{
+		thread_vector[i].thd_id = i;
+	}
+	return 1;
+}
+
+ConflictThreadPool::~ConflictThreadPool()
+{
+	thread_vector.clear();
+}
+
+int ConflictThreadPool::distribute_task(TrxLog * cur_trx_log,ConstTrxlogList &task_content,uint32_t require_threads)
+{
+	if(require_threads <= 0)
+	{
+		require_threads = max_capacity;
+	}
+	uint32_t task_len = task_content.size();
+	if(task_len <= 2*require_threads)
+	{
+		//TODO verify in local thread
+	}
+	else
+	{
+		uint32_t unit_len = task_len / require_threads;	
+
+	}
+
+	return 1;
+}
