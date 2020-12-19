ab71161969f09f8bce7d4e57c641dc53f8724c28
pokemonwei <pokemonwei@126.com>
2020-12-19 09:02:47 +0000

    fix some compile bugs

--- plugin/multi_master_log_plugin/benchmark/benchlog.h
+++ plugin/multi_master_log_plugin/benchmark/benchlog.h
@@ -16,22 +16,22 @@ struct BenchLogRecord
 	uint32_t page_no;
 	uint32_t row_no;
 };
-
-bool is_conflict(const BenchLogRecord & ref,const BenchLogRecord & cur,ConflictLevel level = ConflictLevel::ROW)
-{
-	if(ref.page_no == cur.page_no)
-	{
-		return true;
-	}
-	if(level != ROW)
-	{
-		return false;
-	}
-	if(ref.row_no == cur.row_no)
-	{
-		return true;
-	}
-}
+//
+// bool is_conflict(const BenchLogRecord & ref,const BenchLogRecord & cur,ConflictLevel level = ConflictLevel::ROW)
+// {
+//   if(ref.page_no == cur.page_no)
+//   {
+//     return true;
+//   }
+//   if(level != ROW)
+//   {
+//     return false;
+//   }
+//   if(ref.row_no == cur.row_no)
+//   {
+//     return true;
+//   }
+// }
 }
 
 
